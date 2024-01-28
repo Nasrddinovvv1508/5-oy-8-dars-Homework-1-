f5df42b5f564a23bda2d1f023997fd68acbcaf1d
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-01-28 17:55:32 +0500

    All done

---
+++ index.js
@@ -0,0 +1,102 @@
+
+// 1-MASALA
+// const prices = [12, 13, 54, 1, 4];
+
+// let [firstEl,,,, lastEl] = prices;
+// console.log(firstEl);
+// console.log(lastEl);
+
+
+// 2-MASALA
+// const prices = [12, 13, 54, 1, 4];
+
+// let [a, b, ...res] = prices;
+
+// [a, b] = [b, a];
+// console.log(b);
+// console.log(a);
+
+
+// 3-MASALA
+// const prices = [12, 13, 54, 1, 4];
+// let [, a, b] = prices;
+
+// console.log(a * 2);
+// console.log(b * 2);
+
+
+// 4-MASALA
+// const movie1 = {
+//     name: 'The Matrix',
+//     director: 'The Wachowskis',
+//     year: 1999
+// };
+
+// const movie2 = {
+//     genre: 'Sciens Fiction',
+//     cast: ['Keanu Reeves', 'Laurence Fishburne']
+// };
+
+// let {name, director, year, genre, cast} = movie1;
+
+// localStorage.setItem('res', movie1);
+// document.cookie = 'movie1';
+// sessionStorage.setItem('res', movie1);
+
+
+
+// 6-MASALA
+// const movie1 = {
+//     name: 'The Matrix',
+//     director: 'The Wachowskis',
+//     year: 1999
+// };
+
+// const movie2 = {
+//     genre: 'Sciens Fiction',
+//     cast: ['Keanu Reeves', 'Laurence Fishburne']
+// };
+
+
+// function res(arg1, arg2) {
+//     return {...arg1, ...arg2};
+// }
+
+// console.log(res(movie1, movie2));
+
+
+
+// 8-MASALA
+// const user = {
+//     name: 'John',
+//     age: 23,
+//     country: 'USA',
+//     isActive: true,
+//     lastLogin: new Date()
+// };
+
+// function res(arg) {
+//     return {...arg, telNumber: '120100148'}
+// };
+
+// console.log(res(user));
+
+
+// 9-MASALA
+// const user1 = {
+//     name: 'John',
+//     age: 23,
+//     country: 'USA',
+// };
+
+// const user2 = {
+//     isActive: true,
+//     lastLogin: new Date()
+// };
+
+// function users(arg1, arg2) {
+//     return {...arg1, ...arg2};
+// };
+
+// console.log(users(user1, user2));
+
